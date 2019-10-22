9c5d549d815bfecad734f3f958879737fbfbce21
mc3747 <mc3747@126.com>
2019-10-22 17:19:50 +0800

    添加

---
+++ 3_java版本/02_数组/src/com/mc/main.java
@@ -0,0 +1,10 @@
+package com.mc;
+
+public class main {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int array[] = new int[] {11,22,33};
+		System.out.print(array[2]);
+	
+	}
+}
