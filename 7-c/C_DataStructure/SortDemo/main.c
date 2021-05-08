fae22a0861369a93cf1f12f522e7e916e31ab321
小马 <mc3747@126.com>
2021-05-08 17:17:57 +0800

    排序相关

--- 7-c/C_DataStructure/SortDemo/main.c
+++ 7-c/C_DataStructure/SortDemo/main.c
@@ -4,34 +4,37 @@
 //
 //  Created by kk on 2021/5/7.
 //
-
+/*
+ 普通参考：https://zhuanlan.zhihu.com/p/102420855
+ 优化参考：https://zhuanlan.zhihu.com/p/89330236
+ 分类总结：https://juejin.cn/post/6956624975600025613#heading-16
+ */
 #include <stdio.h>
+#include"sort1.h"
+#include<time.h>
+#include<string.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    srand((unsigned)time(NULL));
+    int arr[15];
+    for (int i = 0; i < 15; i++)
+    {
+        arr[i] = rand() % 120;
+    }
+    show(arr, 15);
+    //冒泡排序
+    BubbleSort(arr, 15);
+    //SelectSort(arr, 15);
+    //InsertSort(arr, 15);
+    //QuickSort(arr, 0, 14);
+    //ShellSort(arr, 15);
+    //MergeSort(arr, 0, 14);
+    //radix_sort(arr, 15);
+//    show(arr, 15);
+//    getchar();
     
+    show(arr, 15);
     return 0;
 }
-//1 冒泡排序
-/*
- 比较相邻的元素。如果第一个比第二个大，就交换他们两个。
- 对每一对相邻元素作同样的工作，从开始第一对到结尾的最后一对。这步做完后，最后的元素会是最大的数。
- 针对所有的元素重复以上的步骤，除了最后一个。
- 持续每次对越来越少的元素重复上面的步骤，直到没有任何一对数字需要比较。
- */
-void bubbleSort(int a[], int n)
-{
-  for(int i =0 ; i< n-1; ++i)
-  {
-    for(int j = 0; j < n-i-1; ++j)
-    {
-      if(a[j] > a[j+1])
-      {
-        int tmp = a[j] ;  //交换
-        a[j] = a[j+1] ;
-        a[j+1] = tmp;
-      }
-    }
-  }
-}
+
+
